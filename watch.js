86eb2e79c4f87b3c7632240831c525ba4c957491
David GABISON <david.gabison@outlook.com>
2020-07-07 15:29:29 +0200

    refactor: added colors on console

--- watch.js
+++ watch.js
@@ -7,6 +7,32 @@ const timeouts = [];
 const child_process = require("child_process");
 const Registry = require("winreg");
 
+const Reset = "\x1b[0m";
+const Bright = "\x1b[1m";
+const Dim = "\x1b[2m";
+const Underscore = "\x1b[4m";
+const Blink = "\x1b[5m";
+const Reverse = "\x1b[7m";
+const Hidden = "\x1b[8m";
+
+const FgBlack = "\x1b[30m";
+const FgRed = "\x1b[31m";
+const FgGreen = "\x1b[32m";
+const FgYellow = "\x1b[33m";
+const FgBlue = "\x1b[34m";
+const FgMagenta = "\x1b[35m";
+const FgCyan = "\x1b[36m";
+const FgWhite = "\x1b[37m";
+
+const BgBlack = "\x1b[40m";
+const BgRed = "\x1b[41m";
+const BgGreen = "\x1b[42m";
+const BgYellow = "\x1b[43m";
+const BgBlue = "\x1b[44m";
+const BgMagenta = "\x1b[45m";
+const BgCyan = "\x1b[46m";
+const BgWhite = "\x1b[47m";
+
 (() => {
   console.nativeLog = console.log;
   console.log = (...args) => console.nativeLog(new Date().toLocaleTimeString(), ...args);
@@ -40,6 +66,7 @@ function handleChange(file) {
     "DLG_Famille_maj_resultats_calcules.py",
     "Extensions_automatiques.py",
     "Mensualite_base.py",
+    "Tests.py",
   ];
   const toIgnore = [
     ".git",
@@ -73,7 +100,7 @@ function handleChange(file) {
   if (toIgnore.includes(file)) return;
   if (file === "NoethysLogIn.ahk") return restartAHK();
   beep();
-  console.log(`Wath to do on change file: ${file} ?`);
+  console.log(`${FgRed}Wath to do on change file: ${file} ?${Reset}`);
 }
 
 function restartAHK() {
